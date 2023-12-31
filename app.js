66dea8390dfa4646d272d08afecb87b95bf52678
Illeauns <nicholasczx@gmail.com>
2023-12-31 15:58:15 -0800

    fixed carousels

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const hamburger = document.querySelector(".hamburger")
-const navMenu = document.querySelector(".nav-menu")
 window.onload = function () {
     const carousels = document.querySelectorAll(".carousel") || [];
 
@@ -96,15 +94,3 @@ window.onload = function () {
         }
     }
 };
-
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active")
-    navMenu.classList.toggle("active")
-})
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active")
-    navMenu.classList.remove("active")
-}))
-
